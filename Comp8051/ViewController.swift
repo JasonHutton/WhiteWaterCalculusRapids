c6fd6739ecf48a29b8b8d53ebbe272c199054508
Nathaniel Zeeman <NathanielZeeman@gmail.com>
2019-04-03 12:00:56 -0700

    missed one

--- Comp8051/ViewController.swift
+++ Comp8051/ViewController.swift
@@ -194,7 +194,7 @@ class ViewController: GLKViewController {
         bottomWall.transform.position = Vector3(x: 0, y: -width, z: 0)
         bottomWall.transform.scale.x = width
         bottomWall.transform.scale.y = 0.5
-        bottomWall.addComponent(component: BlockBody(tag: "Floor8"))
+        bottomWall.addComponent(component: BlockBody(tag: "Floor"))
         bottomWall.addComponent(component: ModelRenderer(modelName: "UnitCube", shader: shader, texture: "winTexture.jpg"))
         GameObject.root.addChild(gameObject: bottomWall)
         
