827e3c88c5965149c030790486b61db06a6f6d54
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2019-04-07 20:12:56 -0700

    Move file loading out of obj loader into its own class

---
+++ Comp8051/TextLoader.swift
@@ -0,0 +1,36 @@
+//
+//  TextLoader.swift
+//  Comp8051
+//
+//  Created by Jason Hutton on 2019-04-07.
+//  Copyright © 2019 Paul. All rights reserved.
+//
+
+import Foundation
+
+class TextLoader {
+
+    public static func loadFile(fileName: String, fileType: String) -> [String] {
+        let string = loadFile(fileName: fileName, fileType: fileType) as String?
+        
+        // split the file data into an array of strings separated by new line character
+        return (string?.components(separatedBy: "\n"))!
+    }
+   
+    public static func loadFile(fileName: String, fileType: String) -> String? {
+    
+        // get the full path for the model file
+        let path = Bundle.main.path(forResource: fileName, ofType: fileType)
+        
+        // get the file using the path
+        let file: FileHandle? = FileHandle(forReadingAtPath: path!)
+        
+        // read the data from the file
+        let data = file?.readDataToEndOfFile()
+        
+        file?.closeFile()
+        
+        // get the contents of the file in one big string
+        return String(data: data!, encoding: String.Encoding.utf8)
+    }
+}
