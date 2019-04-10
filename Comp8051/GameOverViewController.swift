ffb84d843a76b32e23dce68d8fe596d65bca3de2
Paul <mccarliep@gmail.com>
2019-04-10 01:24:27 -0700

    game over tells user how good their score was

--- Comp8051/GameOverViewController.swift
+++ Comp8051/GameOverViewController.swift
@@ -13,8 +13,22 @@ class GameOverViewController: UIViewController {
     
     @IBOutlet weak var scoreLabel: UILabel!
     
+    @IBOutlet weak var message: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let score = ViewController.instance?.score
+        
+        if score! > Settings.instance.getSetting(name: "highScore1") {
+            Settings.instance.setSetting(name: "highScore1", value: score!, explicitSave: true)
+            message.text = "1st Place!"
+        } else if score! > Settings.instance.getSetting(name: "highScore2") {
+            Settings.instance.setSetting(name: "highScore2", value: score!, explicitSave: true)
+            message.text = "2nd Place!"
+        } else if score! > Settings.instance.getSetting(name: "highScore3") {
+            Settings.instance.setSetting(name: "highScore3", value: score!, explicitSave: true)
+            message.text = "3rd Place!"
+        }
         
         scoreLabel.text = "Your Score: \(ViewController.instance!.score)"
     }
