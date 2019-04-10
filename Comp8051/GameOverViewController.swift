4dbe0173690a69708497180de93106fa7bdca7c6
Paul <mccarliep@gmail.com>
2019-04-10 02:08:20 -0700

    Score system fully implemented
    probably

--- Comp8051/GameOverViewController.swift
+++ Comp8051/GameOverViewController.swift
@@ -18,22 +18,31 @@ class GameOverViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let score = ViewController.instance!.score
+        let topScore = Settings.instance.getSetting(name: "highScore1") as Int
+        let secondScore = Settings.instance.getSetting(name: "highScore2") as Int
         
         if score > Settings.instance.getSetting(name: "highScore1") {
             Settings.instance.setSetting(name: "highScore1", value: score, explicitSave: true)
-            MenuViewController.instance?.topScore.text = "Top Score: \(score)"
+            Settings.instance.setSetting(name: "highScore2", value: topScore, explicitSave: true)
+            Settings.instance.setSetting(name: "highScore3", value: secondScore, explicitSave: true)
+            
             message.text = "1st Place!"
         } else if score > Settings.instance.getSetting(name: "highScore2") {
             Settings.instance.setSetting(name: "highScore2", value: score, explicitSave: true)
-            MenuViewController.instance?.secondScore.text = "Second Score: \(score)"
+            Settings.instance.setSetting(name: "highScore3", value: secondScore, explicitSave: true)
+            
             message.text = "2nd Place!"
         } else if score > Settings.instance.getSetting(name: "highScore3") {
             Settings.instance.setSetting(name: "highScore3", value: score, explicitSave: true)
-            MenuViewController.instance?.thirdScore.text = "Third Score: \(score)"
+
             message.text = "3rd Place!"
         }
         
         scoreLabel.text = "Your Score: \(ViewController.instance!.score)"
+        
+        MenuViewController.instance?.topScore.text = "Top Score: \(Settings.instance.getSetting(name: "highScore1") as Int)"
+        MenuViewController.instance?.secondScore.text = "Second Score: \(Settings.instance.getSetting(name: "highScore2") as Int)"
+        MenuViewController.instance?.thirdScore.text = "Third Score: \(Settings.instance.getSetting(name: "highScore3") as Int)"
     }
     @IBAction func backToMenu(_ sender: Any) {
         ViewController.instance?.quit()
