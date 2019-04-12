538b53d7cf61049cdda802fccff6f454840e9543
Paul <mccarliep@gmail.com>
2019-04-12 14:46:08 -0700

    actually works now but is too bright

--- Comp8051/SimpleFragmentShader.glsl
+++ Comp8051/SimpleFragmentShader.glsl
@@ -47,12 +47,12 @@ void main(void) {
     lowp vec3 SpecularColor = u_Light.Color * u_Light.SpecularIntensity * SpecularFactor;
     
     // Point light
-    vec3 lightDir = normalize(u_PointLight.position - frag_Position);
+    lowp vec3 lightDir = normalize(u_PointLight.Position - frag_Position);
     
     // attenuation
-    float distance    = length(u_PointLight.position - fragPos);
-    float attenuation = 1.0 / (u_PointLight.constant + light.linear * distance +
-                               u_PointLight.quadratic * (distance * distance));
+    lowp float dist = length(u_PointLight.Position - frag_Position);
+    lowp float attenuation = 1.0 / (u_PointLight.Constant + u_PointLight.Linear * dist +
+                               u_PointLight.Quadratic * (dist * dist));
     
     // Ambient
     lowp vec3 AmbientColorLava = u_PointLight.Color * u_PointLight.AmbientIntensity;
